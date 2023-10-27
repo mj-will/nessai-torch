00e4a2e295a63a25f600921a1ba70eee1c5fdc4a
mj-will <michaeljw1@googlemail.com>
2023-10-27 15:54:41 +0100

    feat: add option to compile the flow

--- src/nessai_torch/flows.py
+++ src/nessai_torch/flows.py
@@ -1,5 +1,6 @@
 import glasflow
 from glasflow import RealNVP
+import torch
 
 
 def get_realnvp(
@@ -8,12 +9,18 @@ def get_realnvp(
     n_neurons: int,
     n_transforms: int = 4,
     n_layers: int = 1,
+    batch_norm_between_transforms: bool = True,
+    compile: bool = False,
     **kwargs,
 ) -> glasflow.flows.base.Flow:
-    return RealNVP(
+    flow = RealNVP(
         n_inputs=dims,
         n_transforms=n_transforms,
         n_blocks_per_transform=n_layers,
         n_neurons=n_neurons,
+        batch_norm_between_transforms=batch_norm_between_transforms,
         **kwargs,
     )
+    if compile:
+        flow = torch.compile(flow)
+    return flow
