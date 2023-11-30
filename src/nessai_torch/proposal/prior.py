e7cd3424cdbdc0d110138ccd8f6be053508d917b
mj-will <michaeljw1@googlemail.com>
2023-11-30 14:01:11 +0000

    fix: specify device

--- src/nessai_torch/proposal/prior.py
+++ src/nessai_torch/proposal/prior.py
@@ -15,6 +15,6 @@ class PriorProposal(Proposal):
         """Draw a valid sample from the prior"""
         logl = torch.tensor(-torch.inf)
         while not torch.isfinite(logl):
-            x = torch.rand(1, self.dims)
+            x = torch.rand(1, self.dims, device=self.device)
             logl = self.log_likelihood_fn(x)
         return x, logl
