fb026227e72b50d18756e611a7e8ac0b64c75f5c
mj-will <michaeljw1@googlemail.com>
2023-12-22 11:07:06 +0100

    example: update settings and plotting

--- examples/rosenbrock.py
+++ examples/rosenbrock.py
@@ -1,19 +1,20 @@
 """Basic example using the Rosenbrock likelihood."""
 import os
-import time
 
-import matplotlib.pyplot as plt
 import torch
 
 from nessai_torch.sampler import Sampler
+from nessai_torch.plot import corner_plot
 from nessai_torch.utils.bounds import in_bounds
 from nessai_torch.utils.logging import configure_logger
 
+# Limit torch to a single thread
 torch.set_num_threads(1)
+torch.set_num_interop_threads(1)
 
 logger = configure_logger("INFO")
 
-dims = 2
+dims = 4
 bounds = torch.tensor([-5.0, 5.0])
 outdir = "outdir_rosenbrock"
 
@@ -34,26 +35,32 @@ def rosenbrock(x: torch.Tensor) -> torch.Tensor:
     )
 
 
+# Define the log-likelihood
 def log_likelihood(x: torch.Tensor) -> torch.Tensor:
     return torch.log(in_bounds(x, bounds)) - rosenbrock(x)
 
 
+# Define the mapping from the unit hyper-cube to the prior
 def prior_transform(x: torch.Tensor) -> torch.Tensor:
     return (bounds[1] - bounds[0]) * x + bounds[0]
 
 
+# Initialise the sampler
 sampler = Sampler(
     log_likelihood=log_likelihood,
     prior_transform=prior_transform,
     dims=dims,
     outdir=outdir,
     nlive=1_000,
-    constant_volume_fraction=0.95,
-    flow_config=dict(n_neurons=4, batch_norm_between_transforms=True),
+    constant_volume_fraction=0.98,
+    flow_config=dict(n_neurons=32, batch_norm_between_transforms=True),
+    reset_flow=4,
 )
 
+# Run the sampler
 sampler.run()
 
-post = sampler.posterior_samples.cpu().numpy()
-plt.scatter(post[:, 0], post[:, 1])
-plt.savefig(os.path.join(outdir, "posterior.png"))
+# Plot the posterior samples
+corner_plot(
+    sampler.posterior_samples, filename=os.path.join(outdir, "posterior.png")
+)
